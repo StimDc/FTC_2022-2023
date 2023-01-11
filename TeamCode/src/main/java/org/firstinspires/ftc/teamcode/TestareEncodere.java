0db9c34fe2c4a53a23a57d2104703a1ddc5074e4
StimDc <46047627+StimDc@users.noreply.github.com>
2023-01-11 15:37:39 +0200

    Fixed motors not working and added movements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestareEncodere.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestareEncodere.java
@@ -1,64 +1,223 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-@Autonomous(name="TestareEncodere" , group="Linear Opmode")
+
+//the conversion has a 3% error for every 100 cm the robot runs for 103 cm
+
+//TODO: test if robot moves left and right and set constant for distance loss
+//TODO: test if robot rotates left and right and find a way to convert degrees to distance
+
+
+@Autonomous(name="TestareEncodere2022", group="Linear Opmode")
 //@Disabled
-public class Autonomousceva extends LinearOpMode {
+public class Autonomiesemiencoder extends LinearOpMode {
 
-    public DcMotor LB;
-    public DcMotor RB;
-    public DcMotor LF;
-    public DcMotor RF;
-    public float TICKS = 537.7f;
-    public float WHEEL = 11.811f;
-    public float GEAR = 2.0f;
-    public float COUNT = (TICKS * GEAR ) / (WHEEL * 3.1415f);
+    // Declare OpMode members.
+    private DcMotor front_left=null;
+    private DcMotor front_right=null;
+    private DcMotor back_left=null;
+    private DcMotor back_right=null;
+    private DcMotor slider=null;
+    private ElapsedTime runtime = new ElapsedTime();
 
+    private static final double TICKS_PER_REV = 537.7d;
+    private static final double PI = 3.14159265d;
+    //private static final float GEAR_REDUCTION = 2.0f;
+    private static final double  WHEEL_CIRCUMFERENCE =  PI * 3.7795d;
+    // private static final double COUNTS_PER_INCH = (TICKS_PER_REV* GEAR_REDUCTION)/(WHEEL_CIRCUMFERENCE  * PI);
+    private static float distanta =0;
+    private static float pozitie=0;
     @Override
-    public void runOpMode() throws InterruptedException {
-        LB = hardwareMap.dcMotor.get("BL");
-        RB = hardwareMap.dcMotor.get("BR");
-        LF = hardwareMap.dcMotor.get("FL");
-        RF = hardwareMap.dcMotor.get("FR");
+    public void runOpMode() {
 
-        LB.setDirection(DcMotor.Direction.FORWARD);
-        RB.setDirection(DcMotor.Direction.REVERSE);
-        LF.setDirection(DcMotor.Direction.FORWARD);
-        RF.setDirection(DcMotor.Direction.REVERSE);
-        usingEncoders();
-        resetEncoders();
+        telemetry.addData("Status", "Test");
+        telemetry.update();
 
-        float distance;
+        //Links the virtual objets to the real motors
+        front_left=hardwareMap.get(DcMotor.class, "FL");
+        front_right=hardwareMap.get(DcMotor.class, "FR");
+        back_left=hardwareMap.get(DcMotor.class, "BL");
+        back_right=hardwareMap.get(DcMotor.class, "BR");
+
+
+
+        //sets motors direction  (depends on how are the physical motors fixated)
+        front_left.setDirection(DcMotor.Direction.REVERSE);
+        front_right.setDirection(DcMotor.Direction.FORWARD);
+        back_left.setDirection(DcMotor.Direction.REVERSE);
+        back_right.setDirection(DcMotor.Direction.FORWARD);
+        //slider.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        distance = (convertCM(30)/WHEEL) *TICKS;
-        distance = -distance; 
-        RF.setTargetPosition((int)distance);
-        RF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RF.setPower(0.5);
-        while(LF.isBusy()){
-            
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            run_using_encoders();
+            reset_encoders();
+
+
+            forward(100,0.3);
+
+            while(front_left.isBusy()){
+
+            }
+            reset_encoders();
+
+
+
+
+
+            sleep(30000); //after execution, the program will wait until the times end so it doesnt loop
         }
 
+    }
+
+    //sets motors to run with encoders
+    public void run_using_encoders(){
+        front_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //prepares the motors to run to position
+    public void run_to_position(){
+        back_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        back_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        front_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        front_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    //resets encoders for setting new distance
+    public void reset_encoders(){
+        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    //function to convert the distance in centimeters in inches
+
+    public double cm_to_inch(double cm){
+        return (double)(cm/2.54);
+    }
+
+    ///function for the robot to run forward with distance in cm and power
+
+    public void  set_target_position(double distance){
+        distance = cm_to_inch(distance); //converts cm to inch because the formula is in inch;
+        front_left.setTargetPosition((int)((distance/WHEEL_CIRCUMFERENCE)*TICKS_PER_REV));
+        front_right.setTargetPosition((int)((distance/WHEEL_CIRCUMFERENCE)*TICKS_PER_REV));
+        back_left.setTargetPosition((int)((distance/WHEEL_CIRCUMFERENCE)*TICKS_PER_REV));
+        back_right.setTargetPosition((int)((distance/WHEEL_CIRCUMFERENCE)*TICKS_PER_REV));
+    }
+
+    //takes distance power and direction as parameters,
+    //if direction is set to "left" the robot will move to left, if set to "right" the robot will move to right
+
+
+    //dist_loss_const is a constant used for recalculating the distance because when doing the complex moves the robot will lose distance
+    public void left_right(double distance, double power, String dir){
+        //double dist_loss_const = 1;
+        set_target_position(distance);
+        run_to_position();
+
+        if(dir == "left"){
+            power = power;
+        }
+
+        else if(dir == "right"){
+            power = -power;
+        }
+        back_right.setPower(power);
+        back_left.setPower(-power);
+        front_right.setPower(-power);
+        back_left.setPower(power);
 
     }
-    public void usingEncoders(){
+    //takes degrees power and direction as parameters
+    // the program will convert degrees to distance and the robot will turn left or right
+    public void rotate(double degrees, double power, String dir){
+        double deg_to_dis = 1;
 
-        LF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        LB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        double distance = 0;
+        set_target_position(distance);
+        run_to_position();
+
+        if(dir == "left"){
+            power = power;
+        }
+
+        else if(dir == "right"){
+            power = -power;
+        }
+        back_right.setPower(power);
+        back_left.setPower(-power);
+        front_right.setPower(power);
+        back_left.setPower(-power);
     }
-    public void resetEncoders(){
-        //RB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //LB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    //
+    public void forward(double distance,double power){
+
+        set_target_position(distance);
+
+        run_to_position();
+        back_right.setPower(power);
+        back_left.setPower(power);
+        front_left.setPower(power);
+        front_right.setPower(power);
+
+        while(back_left.isBusy() && back_right.isBusy() && front_left.isBusy() && front_right.isBusy()){
+            // waits till the robot finishes moving
+        }
+        Stop();
     }
-    public float convertCM(double cm){
-        return (float)cm/2.54f;
+    ///function
+    public void Stop () {
+        front_left.setPower(0);
+        front_right.setPower(0);
+        back_left.setPower(0);
+        back_right.setPower(0);
     }
 }
+
